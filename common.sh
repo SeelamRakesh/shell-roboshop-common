d53ae150ee065535f4f2d3eb6ee576a1abffdf79
rakesh <rakeshgautam602@gmail.com>
2026-09-09 20:39:59 +0530

    actions

--- common.sh
+++ common.sh
@@ -14,10 +14,9 @@ SCRIPT_START_TIME=$(date +%s)
 
 mkdir -p $LOG_FOLDER
 
-echo "$(date "+%d-%m-%Y %H:%M:%S") Script execution started at $(date)" | tee -a >> LOG_FILE
+echo "$(date "+%d-%m-%Y %H:%M:%S") Script execution started at $(date)" | tee -a $LOG_FILE
 
 check_root(){
- USER_ID=$(id -u)
  if [ $USER_ID -ne 0 ]; then
   echo -e "Run this script as $R Root User $N"
   exit 1
@@ -26,15 +25,15 @@ check_root(){
 
 VALIDATE(){
   if [ $1 -ne 0 ]; then
-   echo -e "$(date "+%d-%m-%Y %H:%M:%S") | $2 $R FAILURE $N" | tee -a >> LOG_FILE
+   echo -e "$(date "+%d-%m-%Y %H:%M:%S") | $2 $R FAILURE $N" | tee -a $LOG_FILE
   else 
-   echo -e "$(date "+%d-%m-%Y %H:%M:%S") | $1 $G SUCCESS $N" | tee -a >> LOG_FILE
+   echo -e "$(date "+%d-%m-%Y %H:%M:%S") | $1 $G SUCCESS $N" | tee -a $LOG_FILE
   fi
 }
 
 print_total_time(){
     SCRIPT_END_TIME=$(date +%s)
     TOTAL_TIME=$(($SCRIPT_START_TIME-$SCRIPT_END_TIME))
-    echo -e "$(date "+%d-%m-%Y %H:%M:%S") | Script executed in $G $TOTAL_TIME $N" | tee -a >> LOG_FILE
+    echo -e "$(date "+%d-%m-%Y %H:%M:%S") | Script executed in $G $TOTAL_TIME $N" | tee -a $LOG_FILE
 }
 
