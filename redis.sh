486415b0e20a42c7c040d01dbe93a7f6752b53ff
rakesh <rakeshgautam602@gmail.com>
2026-09-10 18:59:52 +0530

    actions

--- redis.sh
+++ redis.sh
@@ -5,16 +5,16 @@ source ./common.sh
 check_root
 APP_NAME=redis
 
-dnf module disable redis -y
-dnf module enable redis:7 -y
+dnf module disable redis -y 
+dnf module enable redis:7 -y &>> $LOG_FILE
 VALIDATE $? "Enabling redis:7" 
 
-dnf install redis -y 
+dnf install redis -y &>> $LOG_FILE
 VALIDATE $? "Installing redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/' -e 's/protected-mode c yes/protected-mode no/' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections"
 
-systemctl enable redis 
+systemctl enable redis &>> $LOG_FILE
 systemctl start redis 
 VALIDATE $? "Enabling and starting redis" 
